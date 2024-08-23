f763169b883bd22690e70508ab8db6f878bb2519
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-08-23 15:16:53 +0200

    Add function to generate password

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,20 @@
 # password_generator.py
+#print("Hello, world!")
+  
+#Importation des bibliothèques
+import random
+import string
 
-print("Hello, world!")  
-
+# Fonction pour générer le mot de passe
+def generate_password(length):
+    # Vérifie que la longueur est positive
+    if length <= 0:
+        raise ValueError("La longueur du mot de passe doit être positive.")
+    
+    # Combinaison des caractères possibles
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Utilisation de random.choices pour générer le mot de passe
+    password = ''.join(random.choices(characters, k=length))
+    
+    return password
