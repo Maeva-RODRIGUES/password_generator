04182dcc00d67485505a093bc7fbd7093f53297b
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-08-23 15:24:30 +0200

    Add principal function

--- password_generator.py
+++ password_generator.py
@@ -18,3 +18,25 @@ def generate_password(length):
     password = ''.join(random.choices(characters, k=length))
     
     return password
+
+# Fonction principale pour exécuter le script
+def main():
+    print("Bienvenue dans le générateur de mot de passe !")
+    
+    # Boucle jusqu'à ce que l'utilisateur entre une valeur correcte (capture les erreurs)
+    while True:
+        try:
+            length = int(input("Entrez la longueur du mot de passe : "))
+            if length <= 0:
+                print("Veuillez entrer un nombre entier positif.")
+            else:
+                break
+        except ValueError:
+            print("Veuillez entrer un nombre entier valide.")
+    
+    # Génération et affichage du mot de passe
+    password = generate_password(length)
+    print(f"Votre mot de passe généré est : {password}")
+
+if __name__ == "__main__":
+    main() #assure que le code soit exécuté diirectement et ,on importé comme module
